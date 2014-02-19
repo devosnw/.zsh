910276702711865b1e6c715db940d2ca99327b38
Nic DeVos <nic@devos.me>
2014-02-18 16:36:02 -0800

    Better path setting

--- exports.zsh
+++ exports.zsh
@@ -1,9 +1,13 @@
 # Currently this path is appended to dynamically when picking a ruby version
 # zshenv has already started PATH with rbenv so append only here
-export PATH=$PATH:~/bin:/usr/local/bin:/usr/local/sbin:~/bin
+export PATH=$PATH:$HOME/bin:/usr/local/bin:/usr/local/sbin:$HOME/bin
+export PATH=/usr/local/bin:$PATH
 
 # Set default console Java to 1.6
 # export JAVA_HOME=/System/Library/Frameworks/JavaVM.framework/Versions/1.6/Home
+
+# go
+export GOPATH=$HOME/dev/go
 
 # Setup terminal, and turn on colors
 export TERM=xterm-256color
