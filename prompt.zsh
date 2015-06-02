d06541e0906f04b7592cff8b523511c29f0b7670
Nic DeVos <nic@devos.me>
2015-06-02 12:17:09 -0700

    Improve vcs aware prompt to better support various svn repositories

--- prompt.zsh
+++ prompt.zsh
@@ -3,8 +3,8 @@ autoload -U colors && colors # Enable colors in prompt
 # git prompt colors
 ZSH_THEME_GIT_PROMPT_PREFIX="± ⟫ on "
 ZSH_THEME_GIT_PROMPT_SUFFIX=" ⟪%{$reset_color%}"
-ZSH_THEME_GIT_PROMPT_DIRTY="%{$fg[red]%}"
-ZSH_THEME_GIT_PROMPT_CLEAN="%{$fg[green]%}"
+export ZSH_THEME_GIT_PROMPT_DIRTY="%{$fg[red]%}"
+export ZSH_THEME_GIT_PROMPT_CLEAN="%{$fg[green]%}"
 
 # host
 function get_host() {
@@ -13,51 +13,65 @@ function get_host() {
 
 # returns pwd or ~ if in home
 function get_pwd() {
-   echo $(pwd | sed -e "s,^$HOME,~,")
+   pwd | sed -e "s,^${HOME},~,"
 }
 
 # returns the svn dir
 function svn_status() {
-    # dirty or clean?
-    local dirty="$(svn status --quiet)"
-    local unversioned=$(svn status | grep --color=NEVER '^\?')
+    # dirty or clean? must check for both "modified" items and "unversioned"
+    # items separately to correctly detect dirtiness of repos with externals
+    local dirty
+    local unversioned
+    local color
+    dirty=$(svn status --quiet)
+    unversioned=$(svn status | grep --color=NEVER '^\?')
+
     if [[ -n "${dirty}" ]] || [[ -n "${unversioned}" ]]; then
-        dirty="%{$fg[red]%}"
+        color="%{$fg[red]%}"
     else
-        dirty="%{$fg[green]%}"
+        color="%{$fg[green]%}"
     fi
 
-    # get branch name and revision #
-    local branch=$(svn_get_branch_name)
-    local revision=$(svn info | awk '/^Revision:/{print $2}')
+    # get repo information
+    local info
+    local repo
+    local relative_path
+    local revision
+    info=$(svn info)
+    repo=$(echo "${info}" | awk '/^Repository Root:/{ print $3; }')
+    repo=$(basename "${repo}")
+    relative_path=$(echo "${info}" | awk '/^Relative URL:/{ print $3; }')
+    relative_path="${relative_path:2}"
+    if [[ -n "${relative_path}" ]]; then
+        relative_path=":${relative_path}"
+    fi
+    revision=$(echo "${info}" | awk '/^Revision:/{ print $2; }')
 
     # echo out the status
-    if [[ -n "${branch}" ]]; then
-        echo "$dirty§ ⟫ $branch:$revision ⟪%{$reset_color%}"
-    fi
+    echo "${color}§ ⟫ ${repo}${relative_path}@${revision} ⟪%{$reset_color%}"
 }
 
 # returns the git status
 function git_status() {
-    echo "$(parse_git_dirty)$ZSH_THEME_GIT_PROMPT_PREFIX$(current_branch)$ZSH_THEME_GIT_PROMPT_SUFFIX"
+    echo "$(parse_git_dirty)${ZSH_THEME_GIT_PROMPT_PREFIX}$(current_branch)${ZSH_THEME_GIT_PROMPT_SUFFIX}"
 }
 
 # returns the vcs status
 function get_vcs_status() {
-    # get status
-    svn info 1>/dev/null 2>&1
-    if [[ "$?" -eq 0 ]]; then
-        echo "$(svn_status)"
-    elif [ -d '.git' ] || git rev-parse --git-dir > /dev/null 2>&1; then
-        echo "$(git_status)"
+    if in_svn; then
+        svn_status
+    elif [[ -d '.git' ]] || git rev-parse --git-dir >/dev/null 2>&1; then
+        git_status
     fi
 }
 
 # makes the prompt
 function make_prompt() {
     # set the vars
-    local host="$(get_host)"
-    local pwd="$(get_pwd)"
+    local host
+    local pwd
+    host=$(get_host)
+    pwd=$(get_pwd)
 
     # the prompt
     echo "
